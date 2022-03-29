d12cb460926d78ba588928fdb1e2796dad6dc09b
Prithwis Das <prithwisdas2017@gmail.com>
2022-03-29 19:10:04 +0530

    updated code and csv files for analysis

---
+++ main.py
@@ -0,0 +1,15 @@
+#imports
+import numpy as np
+from sklearn import svm
+from sklearn.svm import SVR
+import pandas as pd
+from pandas import read_csv
+import matplotlib.pyplot as plt
+import random
+from random import randrange
+from csv import writer
+
+#main code
+df = read_csv("top_pandemics.csv")
+
+print(df["Death"])
