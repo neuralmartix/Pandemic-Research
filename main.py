2c6be495276f48760b2f70121e00cc5c00bb730c
Prithwis Das <prithwisdas2017@gmail.com>
2022-03-30 07:23:17 +0530

    update code

--- main.py
+++ main.py
@@ -13,33 +13,54 @@ from csv import writer
 df = read_csv("top_pandemics.csv")
 
 y_initial = [] 
-y_final = [] #y-coordinate
+y_final = [] 
+y = [] #y-coordinate
 
-for deaths in df["Death"]:
-    l = deaths.split(" ")
+for pop in df["Global population lost"]:
+    l = pop.split("[")
     y_initial.append(l[0])
 
 for elements in y_initial:
     if "–" in elements:
         filter = elements.split("–")
-        if "." in filter[1]:
-            y_final.append(float(filter[1]))
-        else:
-            y_final.append(int(filter[1]))    
+        y_final.append(filter[1])
     else:
-        if "." in elements:
-            y_final.append(float(elements))
+        y_final.append(elements) 
+
+for elements in y_final:
+    if elements != "":
+        if "." not in elements:
+            y.append(int(elements[:-1]))
         else:
-            y_final.append(int(elements))  
+            y.append(float(elements[:-1]))
+    elif elements == "":
+        y.append("Unknown")
 
-print(y_final)
+print(y)
 
-print(df["Date"])
+x_initial = []
+x = []
 
+for dates in df["Date"]:
+    date = str(dates)
+    d = date.split("–")
+    x_initial.append(d[0])
+
+for elements in x_initial:
+    if "[" in elements:
+        d = elements.split("[")
+        x.append(d[0])
+    else:
+        x.append(elements)    
+
+#print(x_initial)
+print(x)
+
+"""
 x = []
 for elements in df["Disease"]:
     x.append(elements)
         
-plt.bar(x,y_final) 
-plt.show()
-       
+plt.bar(x,y) 
+plt.show() 
+"""
