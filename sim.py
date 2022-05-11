99161b1562e60f708457f3a916eb7bf3fd8a71e3
Prithwis Das <prithwisdas2017@gmail.com>
2022-05-11 23:30:12 +0530

    update

---
+++ sim.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn.svm import SVR
+from pandas import read_csv
+import matplotlib.pyplot as plt
+import random
+from random import randrange
+
+years = 0
+n_epochs = 100000
+res1 = []
+data1 = []
+for i in range(n_epochs):
+  year = 0
+  #probs = float(randrange(325,496))/1000
+  while True:
+    rand = random.uniform(0,1)
+    if rand > 0.496:
+      print(years/n_epochs)
+      break
+    else:
+      years += 1
+      year += 1
+
+#print(years/n_epochs)
